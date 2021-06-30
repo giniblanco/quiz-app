b70cf9b8ccf242d9e9c989bde25cf3729bff4d0e
giniblanco <78448138+giniblanco@users.noreply.github.com>
2021-06-30 17:11:44 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// Array of objects with our quiz questions & answers
 const quizData = [
     {
         question: 'When did The Hellacopters form?',
@@ -33,13 +35,14 @@ const quizData = [
     },
 ]
 
-// get the elements and put them into variables
-
+// Container for our current question
 let currentQuestion = 0;
 
+// 1) LOAD QUESTION
 loadQuestion();
 
 function loadQuestion(){
+    
     const currentQuestionData = quizData[currentQuestion];
 
     document.querySelector("#question").innerHTML = 
@@ -58,55 +61,78 @@ function loadQuestion(){
     currentQuestionData.d;
 }
 
+// 2) CLICK SUBMIT, 3) VALIDATE ANSWER, 4) GET SELECTED ANSWER,
+// 5) DESELECT ANSWER, 6) SHOW RESULTS PAGE, 7) RELOAD QUIZ
 document.querySelector("#submit").addEventListener("click",
 () => {
-
-    getSelectedAnswer();
-    currentQuestion++;
-
-    if (currentQuestion < quizData.length){
-        loadQuestion();
-    } else {
-        document.querySelector(".quiz-header").innerHTML = 
-        `<h1>${average}%</h1>
-        <h2 id="finalMessage">Good job! Get yourself an 
-        orange lemonade! 🍹</h2>`;
-        document.querySelector("#submit").remove();
-    }
-}
-)
+        let answerValid = answerValidator();
+        
+        if (answerValid == false) {
+            loadQuestion();
+        } else {
+                getSelectedAnswer();
+                currentQuestion++;  
+                
+                if (currentQuestion < quizData.length){
+                deselectAnswer();
+                loadQuestion();
+                } else {
+                    document.querySelector(".quiz-header").innerHTML = 
+                    `<h1>${average}%</h1>
+                    <h2 id="finalMessage">Good job! Get yourself an 
+                    orange lemonade! 🍹</h2>`;
+                    document.querySelector("#submit").innerHTML =
+                    "Play again 😊";
+                    document.querySelector("#submit").setAttribute
+                    ("onClick", "location.reload()");
+                }
+        }
+})
 
 let point = 0;
 let average = 0;
 
+// 4) GET SELECTED ANSWER
 function getSelectedAnswer (){
     const currentQuestionData = quizData[currentQuestion];
     const answers = document.querySelectorAll(".answer");
     
-        answers.forEach(answers => {
-        
-        if (answers.checked) {
-            console.log(answers.checked);
-            console.log(answers.value);
-            console.log(currentQuestionData.correct);
-            
-            if (answers.value == currentQuestionData.correct){
-                console.log("correct!");
-                point = point + 1;
-                console.log(point);
-                average = (point / quizData.length) * 100;
-                console.log(average); 
-                return average;
-            }    
-        }
-    })
+        answers.forEach((answers) => {
+            if (answers.checked) {                           
+                if (answers.value == currentQuestionData.correct){
+                    point = point + 1;
+                    average = (point / quizData.length) * 100;
+                    return average;
+                }    
+            }           
+        });         
 }
 
+// 5) DESELECT ANSWER
+function deselectAnswer(){
+    const answers = document.querySelectorAll(".answer");
+    answers.forEach((answers) => {
+        answers.checked = false;
+    });
+}
 
+// 3) VALIDATE ANSWER
+function answerValidator(){
 
+    let answers = document.querySelectorAll(".answer");
+    let answerValid = false;
+    
+    let i = 0;
+    while (!answerValid && i < answers.length) {
+        if (answers[i].checked) {
+            answerValid = true;
+        }
+        i++;
+    }
 
-
-
-
-
-
+    if (!answerValid && currentQuestion != 4) {
+        alert("Must check some option!")
+    }
+    
+    return answerValid;
+}
